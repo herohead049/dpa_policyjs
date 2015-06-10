eba3b3e6f2a831553adde592508f6e5458c1dea1
herohead049 <herohead@gmail.com>
2015-06-10 14:00:00 -0400

    c

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
 var cdlib = require('cdlib');
 
 
+var argv = require('minimist')(process.argv.slice(2));
+
+
+var dpaArgs = {
+    node: "",
+    message: "",
+    severity: "",
+    eventName: "",
+    alertID: "",
+    eventID: "",
+    firstOccurrence: "",
+    lastOccurrence: "",
+    count: "",
+    sourceView: "",
+    sourceNode: "",
+    rule: ""
+};
+
+dpaArgs.node = argv._[0];
+dpaArgs.message = argv._[1];
+dpaArgs.severity = argv._[2];
+dpaArgs.eventName = argv._[3];
+dpaArgs.alertID = argv._[4];
+dpaArgs.eventID = argv._[5];
+dpaArgs.firstOccurrence = argv._[6];
+dpaArgs.lastOccurrence = argv._[7];
+dpaArgs.count = argv._[8];
+dpaArgs.sourceView = argv._[9];
+dpaArgs.sourceNode = argv._[10];
+dpaArgs.rule = argv._[11];
+
+
+console.log(dpaArgs);
